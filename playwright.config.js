0a29e4825db08bb0ce80e43f603e151fe63ce240
Anonym <nmk8rf2kwt@privaterelay.appleid.com>
2026-01-11 17:33:49 +0100

    fix(ci): start web server in playwright config

--- playwright.config.js
+++ playwright.config.js
@@ -16,11 +16,18 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        /* Base URL to use in actions like `await page.goto('/')`. */
         baseURL: 'http://localhost:10000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
+    },
+
+    /* Run your local dev server before starting the tests */
+    webServer: {
+        command: 'npx http-server -p 10000 .',
+        port: 10000,
+        reuseExistingServer: !process.env.CI,
+        timeout: 120 * 1000,
     },
 
     /* Configure projects for major browsers */
